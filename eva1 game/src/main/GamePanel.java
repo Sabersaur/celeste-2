aaa1f133e363267d52e113a599d21664fd5580d5
Sabersaur <antonemilbork@gmail.com>
2023-04-15 12:39:06 +0200

    Update GamePanel.java
    commenting

--- eva1 game/src/main/GamePanel.java
+++ eva1 game/src/main/GamePanel.java
@@ -1,7 +1,7 @@
 package main;
 
 import java.awt.Color;
-//import java.awt.BasicStroke; // For debugging... or, it was. Somehow, it is no longer needed. I dare not remove it though
+//import java.awt.BasicStroke; //For debugging...
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,7 +14,7 @@ import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable{
 	
-	// world settings
+	//world settings
 	public final int maxWorldCol = 40;
 	public final int maxWorldRow = 23;
 	public final float scaleWindow = (float) 1;
@@ -29,38 +29,41 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	public TileManager tileM = new TileManager(this);
 	KeyHandler KeyH = new KeyHandler();
-	Thread gameThread; 
+	Thread gameThread; /*subclass Thread gameThread; : bliver ved med at kører indtil det bliver stoppet. 
+	For at kører tiden i programmet skal man implimenter "Runnable"*/
 	public Player player = new Player(this,KeyH);
 	
 	long Timer = System.nanoTime();
 
 	public GamePanel () {
 		
-		this.setPreferredSize(new Dimension((int) screenWidth,(int) screenHeight));
-		this.setBackground(Color.black);
-		this.setDoubleBuffered(true);
-		this.addKeyListener(KeyH);
-		this.setFocusable(true);	
+		this.setPreferredSize(new Dimension((int) screenWidth,(int) screenHeight)); //sætter størelse på JPanal klassen
+		this.setBackground(Color.black); //sætter bagrunden af vinduet til farven sort
+		this.setDoubleBuffered(true); //alle tegninger i komponetet bliver tegnet offscreen i en buffer, det vil derfor gøre rendering bedre i JPanal
+		this.addKeyListener(KeyH); // Det gør at Gamepanel klassen kan se KeyInput fra KeyHandler
+		this.setFocusable(true);	//uptimization for KeyInputs
 	}
 
 	public void startGameThread() {
 		
-		gameThread = new Thread(this);
-		gameThread.start();
+		gameThread = new Thread(this); //"this" betyder GamePanel klassen (passing to GamePanel klassen)
+		gameThread.start(); //starter update tiden i programmet
 	}
 
-	@Override
+	@Override 
+	/*(methode): når et object (GamePanel) implimentere "Runnable" bliver "void run" brugt til at starte en Thread. 
+	så når Thread'en starter kalder den Run metoden. */
 	public void run() {
 		
-		double drawInterval = 1000000000/FPS; //0.01666 sec
-		double nextDrawTime = System.nanoTime() + drawInterval;
+		double drawInterval = 1000000000/FPS; //0.01666 sec = 60 times på et sek
+		double nextDrawTime = System.nanoTime() + drawInterval; 
 		
- 		
+ 		//while Loop repeater det inde i loopet mens og så længde gameThread kører
 		while(gameThread !=null) {
 			
-			update();
+			update(); //updater player position, som bliver til movement i spillet
 			
-			repaint();
+			repaint(); //repaint er måden man kalder paintcomponet metoden i java
 			
 			try {
 				double remainingTime = nextDrawTime - System.nanoTime();
@@ -81,27 +84,32 @@ public class GamePanel extends JPanel implements Runnable{
 			
 		}
 	}
+	//kalder update metoden
 	public void update() {
 		
 		player.update();
 		
 	}
+	//paintComponent()  (metode) : standard build in metode i java til at tegne i JPanel
+	//(Graphics g) En klasse der her mange funktioner til at tegne objekter i vinduet
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
 		
-		Graphics2D g2 = (Graphics2D)g;
+		Graphics2D g2 = (Graphics2D)g; /*Graphics2D : er en klasse der extender funktionaliteten af Graphics klassen, 
+		til at give mere kontrol over movement cordinater i 2D og farver og tekst layout.*/
+		//Graphics2D g2 = (Graphics2D)g; : betyder at vi laver klassen om til Graphics2D klassen.
 
 		tileM.draw(g2);
 		
 		player.draw(g2);
-		g2.setColor(Color.black);
-		g.fillRect (60, 57, 100, 30);
-		g2.setColor(Color.white);
-		g2.drawString(String.valueOf((System.nanoTime()-Timer)/1000000),75,75);
+		g2.setColor(Color.black); //sætter 2D farven sort
+		g.fillRect (60, 57, 100, 30); //rectangle med given størrelse
+		g2.setColor(Color.white); //sætter rect til hvid
+		g2.drawString(String.valueOf((System.nanoTime()-Timer)/1000000000),75,75);
 	
 	
-		g2.dispose();
+		g2.dispose();//uptimizeastion
 		
 	}
 }
