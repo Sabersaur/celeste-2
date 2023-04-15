586896d89aa355a8be2ef4dfa92e7ec562f233e9
Sabersaur <antonemilbork@gmail.com>
2023-04-15 13:12:16 +0200

    comments
    extra comments

--- eva1 game/src/main/KeyHandler.java
+++ eva1 game/src/main/KeyHandler.java
@@ -4,21 +4,22 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener{
-	
+	//Keylitsener er et interface for Keyboard inputs
 	public boolean upPressed, downPressed, leftPressed, rightPressed, xKey, xKeyBad, spacePressed;
 
-	@Override
+	@Override //keyTyped er ikke så vigtig for vores program
 	public void keyTyped(KeyEvent e) {
 		
 	}
 
-	@Override 
+	@Override //KeyPressed : når du trykker på en key på dit Keyboard
 	public void keyPressed(KeyEvent e) {
 		
-		int code = e.getKeyCode();
+		int code = e.getKeyCode(); //int code = e.getKeycode : retunere integeren af Keycoden som et nummer af en pressed key
 		
 		if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
-			upPressed = true;
+			upPressed = true; /*if(code == KeyEvent.VK_W)...osv : if statement om en user har trykket på "W"
+			then set upPressed = true; : Programmet ser du har trykket på knappen*/
 		}
 		if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
 			downPressed = true;
--- eva1 game/src/main/main.java
+++ eva1 game/src/main/main.java
@@ -6,15 +6,15 @@ public class main {
 
 	public static void main(String[] args) { //Constant warning "This method has a constructer name". Can we fix this?
 
-		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.setTitle("2D Game");
+		JFrame window = new JFrame(); //skaber en windowbrowser på din computer.
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //lader dig lukke vinduet på din comnputer (x) button
+		window.setResizable(false); // gør så du ikke kan resize dit window
+		window.setTitle("2D Game"); // laver en title til vinduet
 		
 		GamePanel gamePanel = new GamePanel();
-		window.add(gamePanel);
+		window.add(gamePanel); //adder gamepanel funktionerne til vinduet på pc
 		
-		window.pack();
+		window.pack(); //sætter vinduet til den preferred størrelse
 		
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
